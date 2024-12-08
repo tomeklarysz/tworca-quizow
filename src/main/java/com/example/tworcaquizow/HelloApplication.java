2cb3f9d44aa60892c5bc2778908299c6e2d842e4
Karol Arabasz <karolarabasz@student.agh.edu.pl>
2024-12-08 20:05:15 +0100

    Logowanie

--- src/main/java/com/example/tworcaquizow/HelloApplication.java
+++ src/main/java/com/example/tworcaquizow/HelloApplication.java
@@ -11,8 +11,8 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("LoginPanel.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 600, 400);
-        stage.setTitle("Hello!");
+        Scene scene = new Scene(fxmlLoader.load(), 800, 600);
+        stage.setTitle("TworcaQuizow");
         stage.setScene(scene);
         stage.show();
     }
--- src/main/java/com/example/tworcaquizow/HelloController.java
+++ src/main/java/com/example/tworcaquizow/HelloController.java
@@ -3,17 +3,70 @@ package com.example.tworcaquizow;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.paint.Color;
+
+import java.sql.SQLException;
+
+import java.sql.Connection;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
+    String DATABASE_URL = "jdbc:sqlite:/home/karol/IdeaProjects/tworca-quizow/src/main/databases/db_login.db";
 
     @FXML
+    private Label statusLabel;
+    @FXML
+    private TextField loginField;
+    @FXML
+    private TextField passwordField;
+    @FXML
+
     protected void loginButton(ActionEvent event) {
-        welcomeText.setText("Welcome to JavaFX Application!");
+        String login = loginField.getText();
+        String password = passwordField.getText();
+
+        try (Connection conn = Login_DataBase.connectToDatabase(DATABASE_URL)) {
+            if (Login_DataBase.check_account(conn, login, password)) {
+                statusLabel.setText("Zalogowano");
+                statusLabel.setTextFill(Color.GREEN);
+                loginField.clear();
+                passwordField.clear();
+            }
+            else {
+                statusLabel.setText("Nie ma takiego konta");
+                statusLabel.setTextFill(Color.RED);
+                loginField.clear();
+                passwordField.clear();
+            }
+        }
+        catch (SQLException e) {
+            System.out.println("Błąd: " + e.getMessage());
+        }
     }
     @FXML
-    protected void registerButton(ActionEvent event) {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    public void registerButton(ActionEvent event) {
+        String login = loginField.getText();
+        String password = passwordField.getText();
+
+        try (Connection conn = Login_DataBase.connectToDatabase(DATABASE_URL)) {
+            Login_DataBase.createTableIfNotExists(conn);
+            if (Login_DataBase.check_logins(conn, login) == true) {
+                statusLabel.setText("Ten login już istnieje");
+                statusLabel.setTextFill(Color.RED);
+                loginField.clear();
+                passwordField.clear();
+            }
+            else {
+                Login_DataBase.addUserData(conn, login, password);
+                statusLabel.setText("Konto zostało dodane poprawnie");
+                statusLabel.setTextFill(Color.GREEN);
+                loginField.clear();
+                passwordField.clear();
+            }
+        }
+        catch (SQLException e) {
+            System.out.println("Błąd: " + e.getMessage());
+        }
     }
 }
