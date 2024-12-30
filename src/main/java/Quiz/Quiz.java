3daed8418a9640942f6e50d3a3fe299dc261f12c
tomeklarysz <ttomek.larysz@gmail.com>
2024-12-30 19:47:11 +0100

    Klasy Quiz i QuizyDatabase

---
+++ src/main/java/Quiz/Quiz.java
@@ -0,0 +1,27 @@
+package Quiz;
+
+public class Quiz {
+
+    private String name;
+    private String description;
+
+    public Quiz(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
--- src/main/java/com/example/tworcaquizow/QuizyDatabase.java
+++ src/main/java/com/example/tworcaquizow/QuizyDatabase.java
@@ -2,14 +2,18 @@ package com.example.tworcaquizow;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class QuizyDatabase {
-//    public static final String DATABASE_URL = "jdbc:sqlite:/home/karol/IdeaProjects/tworca-quizow/src/main/databases/db_login.db";
-    public static final String DATABASE_URL = "jdbc:sqlite:C:/Users/tomek/IdeaProjects/tworca-quizow/src/main/databases/db_login.db";
+    public static final String DATABASE_URL = "jdbc:sqlite:/home/karol/IdeaProjects/tworca-quizow/src/main/databases/db_login.db";
 
     public static Connection connectToDatabase() throws SQLException {
         return DriverManager.getConnection(DATABASE_URL);
+    }
+
+    public static void loadQuiz(int id, String name) throws SQLException {
+//        do dokonczenia
     }
 
 }
