5d2bf4c85646f5cf630d50a27d4ff221286a7a32
Karol <karolarabasz6@gmail.com>
2024-12-30 16:31:42 +0100

    Dodanie otwierania nowego okna

--- src/main/java/com/example/tworcaquizow/LoginController.java
+++ src/main/java/com/example/tworcaquizow/LoginController.java
@@ -1,10 +1,16 @@
 package com.example.tworcaquizow;
 
+import Uzytkownik.Uzytkownik;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.paint.Color;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import java.io.IOException;
 
 import java.sql.SQLException;
 
@@ -19,21 +25,24 @@ public class LoginController {
     private TextField loginField;
     @FXML
     private TextField passwordField;
-    @FXML
+    private Uzytkownik uzytkownik;
 
-    protected void loginButton(ActionEvent event) {
+    public void loginButton(ActionEvent event) {
         String login = loginField.getText();
         String password = passwordField.getText();
 
         try (Connection conn = LoginDataBase.connectToDatabase(DATABASE_URL)) {
             if (LoginDataBase.check_account(conn, login, password)) {
+                uzytkownik = new Uzytkownik(login, password);
                 statusLabel.setText("Zalogowano");
+                uzytkownik.uzytkownikZalogowany();
                 statusLabel.setTextFill(Color.GREEN);
                 loginField.clear();
                 passwordField.clear();
             }
             else {
                 statusLabel.setText("Nie ma takiego konta");
+                uzytkownik.uzytkownikNieZalogowany();
                 statusLabel.setTextFill(Color.RED);
                 loginField.clear();
                 passwordField.clear();
@@ -42,6 +51,26 @@ public class LoginController {
         catch (SQLException e) {
             System.out.println("Błąd: " + e.getMessage());
         }
+        if (uzytkownik.getStanZalogowania()) {
+            try {
+                // Załaduj nowe okno
+                FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("MainPanel.fxml"));
+                Parent root = fxmlLoader.load();
+
+                // Utwórz nową scenę i okno
+                Stage newStage = new Stage();
+                newStage.setTitle("Main Panel");
+                newStage.setScene(new Scene(root));
+                newStage.show();
+
+                // Zamknij bieżące okno
+                Stage currentStage = (Stage) loginField.getScene().getWindow(); // Pobierz bieżący Stage
+                currentStage.close();
+            } catch (IOException e) {
+                System.out.println("Błąd podczas ładowania pliku FXML: " + e.getMessage());
+            }
+        }
+
     }
     @FXML
     public void registerButton(ActionEvent event) {
